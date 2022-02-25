cf25ca808f183046360f64aec4e17c83cf55b10a
alunos <alunos@etehort.ps.sp>
2022-02-25 20:04:58 -0300

    switch,case para as operações aritméticas

--- Calculadora1/Calculadora/Program.cs
+++ Calculadora1/Calculadora/Program.cs
@@ -7,13 +7,35 @@ namespace Calculadora
         static void Main(string[] args)
         {
         	double Num1, Num2;
-		Console.WriteLine("---- Calculadora ----");
-		Console.WriteLine("Numero 1: ");
-		// double Salary = Convert.ToDouble(Console.ReadLine());
-		Num1 = Convert.ToDouble(Console.ReadLine());
-		Console.WriteLine("Numero 2: ");
-		Num2 = Convert.ToDouble(Console.ReadLine());
-		Console.WriteLine("Selecione uma operação: ");
+            double resultado = 0;
+    		Console.WriteLine("---- Calculadora ----");            
+	    	Console.WriteLine("Numero 1: ");
+		    // double Salary = Convert.ToDouble(Console.ReadLine());
+		    Num1 = Convert.ToDouble(Console.ReadLine());
+		    Console.WriteLine("Numero 2: ");
+		    Num2 = Convert.ToDouble(Console.ReadLine());
+		    Console.WriteLine("Selecione uma operação: +, -, *, /");
+            string op = Console.ReadLine();
+
+            switch(op)
+            {
+                case "+":
+                    resultado = Num1 + Num2;
+                    break;
+                case "-":
+                    resultado = Num1 - Num2;
+                    break;
+                case "*":
+                    resultado = Num1 * Num2;
+                    break;
+                case "/":
+                    resultado = Num1 / Num2;
+                    break;
+                default:
+                    Console.WriteLine("Insira um operador válido.");
+                    break;
+            }
+            Console.WriteLine("" + Num1 + " " + op + "  " + Num2 + " = " + resultado);
         }
     }
 }

da79d7129bc7f5a765d6cb1fbe4e78aab1888c8a
Arley <arley.costa@etec.sp.gov.br>
2022-02-25 16:39:17 -0300

    Mensagens e tipos foram criados

---
+++ Calculadora1/Calculadora/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	double Num1, Num2;
+		Console.WriteLine("---- Calculadora ----");
+		Console.WriteLine("Numero 1: ");
+		// double Salary = Convert.ToDouble(Console.ReadLine());
+		Num1 = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Numero 2: ");
+		Num2 = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Selecione uma operação: ");
+        }
+    }
+}
